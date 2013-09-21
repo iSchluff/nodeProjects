8eea426de8a1322e203b8c3a3002abd6ff62ecb2
Anton Schubert <anton.schubert@gmx.de>
2013-09-21 02:59:48 +0200

    update proxy

--- proxy/app.js
+++ proxy/app.js
@@ -2,18 +2,18 @@ var httpProxy = require('http-proxy');
 
 var options = {
   // this list is processed from top to bottom, so '.*' will go to
-  // '127.0.0.1:3000' if the Host header hasn't previously matched
+  // '127.0.0.1:80' if the Host header hasn't previously matched
   router : {
-    "experiments.cookie-factory.de": "127.0.0.1:3000",
-    "gitlab.cookie-factory.de": "127.0.0.1:3001",
-    ".*": "127.0.0.1:3000"
+    "experiments.cookie-factory.de": "127.0.0.1:80",
+    "gitlab.cookie-factory.de": "127.0.0.1:3000",
+    ".*": "127.0.0.1:80"
   }
 };
 
 // bind to port 80 on the specified IP address
 try {
-  var s=httpProxy.createServer(options).listen(9000, '0.0.0.0');
-  console.log('Proxy on Port 9000');
+  var s=httpProxy.createServer(options).listen(8080);
+  console.log('Proxy on Port 8080');
   s.on("error", function(err){
     console.log(err);
   })
